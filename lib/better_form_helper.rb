e34b4f760a2dc8f0a10ad87fea01ee10f41a1faa
Parker <ops+githubfi@pivotallabs.com>
2009-12-13 08:30:20 -0800

    simple select

--- lib/better_form_helper.rb
+++ lib/better_form_helper.rb
@@ -1,12 +1,4 @@
 module BetterFormHelper
-  def better_form_field(form, obj, field, field_type, label, required=false, help_text='', options = {})
-    content_tag('fieldset', :class => field_type) do
-      content_tag('span', help_text, :class => 'help-text') +
-        form.label(field, label + required(required)) +
-        form.send(field_type, field, options) +
-        better_error_message_for(obj, field)
-    end
-  end
 
   def better_form_for(obj, opts={}, &block)
     opts[:html] ||= {}
@@ -17,13 +9,10 @@ module BetterFormHelper
     concat("<div class='form-bottom'></div>")
   end
 
-  def cancel_and_submit(form, cancel_path, submit_text="Save")
-    link_to('Cancel', cancel_path, :class => 'cancel') +
-      form.submit(submit_text, :id => 'commit')
-  end
-
-  def required(x)
-    "<spam class='required-field'>*</span>"
+  def better_select(form, obj, field, options, label, required=true)
+    standard_form_field(form, obj, field, label, colonize(label), required) do
+      form.select(field, options)
+    end
   end
 
   def better_text_field(form, obj, field, label, required=false, help_text='')
@@ -38,8 +27,26 @@ module BetterFormHelper
     better_form_field(form, obj, field, :password_field, colonize(label), required, '', options)
   end
 
+  def better_form_field(form, obj, field, field_type, label, required=false, help_text='', options = {})
+    content_tag('fieldset', :class => field_type) do
+      content_tag('span', help_text, :class => 'help-text') +
+        form.label(field, label + required(required)) +
+        form.send(field_type, field, options) +
+        better_error_message_for(obj, field)
+    end
+  end
+
+  def cancel_and_submit(form, cancel_path, submit_text="Save")
+    link_to('Cancel', cancel_path, :class => 'cancel') +
+      form.submit(submit_text, :id => 'commit')
+  end
+
   def colonize(label)
     label =~ /[:\?]$/ ? label : "#{label}:"
+  end
+
+  def required(required=true)
+    required ? content_tag('span', '*', :class => 'required-field') : ''
   end
 
   def better_error_message_for(object, method, *args)
